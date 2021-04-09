15bff352d40fc05022260a4e07d56441d0661b89
Bryce Cogswell <bryceco@yahoo.com>
2021-04-09 16:21:22 -0700

    Initial Commit

---
+++ OpeningHoursPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpeningHoursPhoto
+//
+//  Created by Bryce Cogswell on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpeningHoursPhoto/OpeningHoursPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningHoursPhotoApp.swift
+//  OpeningHoursPhoto
+//
+//  Created by Bryce Cogswell on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct OpeningHoursPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
